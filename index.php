e9ed055220cbb5322f85fd8ea6bbff175fa3541c
Tornike Shavidze <ShavidzeT@Gmail.com>
2015-12-29 23:58:08 +0400

    creating and adding test html in index.php

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Startup</title>
+</head>
+<body>
+
+<div id="wrapper">
+	
+	<div id="steps">
+		
+		<div class="step" step="1">
+			hello world1
+		</div>
+
+		<div class="step" step="2">
+			hello world2
+		</div>
+
+		<div class="step" step="3">
+			hello world3
+		</div>
+
+	</div>
+
+</div>
+
+</body>
+</html>
