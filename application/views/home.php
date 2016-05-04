4b3358343108756ffe07d49fc88956bf8eb4fb7d
Tornike Shavidze <shavidzet@gmail.com>
2016-05-04 23:27:29 +0400

    temp3 update

--- application/views/home.php
+++ application/views/home.php
@@ -32,10 +32,10 @@
 
 	<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
 	<script src="<?=base_url()?>assets/js/jquery.min.js"></script>
-	<script src="<?=base_url()?>assets/js/jquery.mobile.min.js"></script>
+	
 	
 	<!-- Include all compiled plugins (below), or include individual files as needed -->
-	<script src="<?=base_url()?>assets/js/bootstrap.min.js"></script>
 	
+
 </body>
 </html>
